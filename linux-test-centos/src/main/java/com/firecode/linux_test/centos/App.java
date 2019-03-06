282571920728a3326681bfec8fa57d309837f0ab
jiangcihuo <chiang-fire@outlook.com>
2019-03-06 10:38:55 +0800

    first commit

---
+++ linux-test-centos/src/main/java/com/firecode/linux_test/centos/App.java
@@ -0,0 +1,12 @@
+package com.firecode.linux_test.centos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
